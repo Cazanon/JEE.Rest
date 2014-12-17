d25fd14d9460f62d000f9d80a68a79b85b23c329
miw-upm <miw@eui.upm.es>
2014-12-17 11:38:15 +0100

    Instalado log4j

--- src/main/java/utils/HelloRest.java
+++ src/main/java/utils/HelloRest.java
@@ -5,11 +5,14 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.apache.logging.log4j.LogManager;
+
 @Path("/hello")
 public class HelloRest {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String msg() {
+        LogManager.getLogger(HelloRest.class).info("GET/ Hello");
         return ">>>Hola, desde RESTful";
     }
 }
