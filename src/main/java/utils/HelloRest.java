b508ba5a431176bbd7e8ea602a69788df521ce77
miw-upm <miw@eui.upm.es>
2014-12-17 11:09:28 +0100

    Initial

---
+++ src/main/java/utils/HelloRest.java
@@ -0,0 +1,15 @@
+package utils;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class HelloRest {
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String msg() {
+        return ">>>Hola, desde RESTful";
+    }
+}
