9d9a037310454f75c77a9034afd79b2474e7ef78
miw-upm <miw@eui.upm.es>
2014-12-20 11:05:34 +0100

    update rest v1

--- src/main/java/utils/v0/RestClient.java
+++ src/main/java/utils/v0/RestClient.java
@@ -18,14 +18,18 @@ public class RestClient {
 
         Invocation.Builder invocation = webTarget.request(MediaType.TEXT_PLAIN);
 
-        Response result = invocation.get();
+        Response response = invocation.get();
 
         LogManager.getLogger(RestClient.class).info("GET/");
-        LogManager.getLogger(RestClient.class).info("-- toString: " + result.toString());
-        LogManager.getLogger(RestClient.class).info("-- Headers: " + result.getHeaders());
-        LogManager.getLogger(RestClient.class).info("-- Status: " + result.getStatus());
-        LogManager.getLogger(RestClient.class).info("-- Entity: " + result.readEntity(String.class));
-        LogManager.getLogger(RestClient.class).info("-- Links: " + result.getLinks());
-        LogManager.getLogger(RestClient.class).info("-- Location: " + result.getLocation());
+        LogManager.getLogger(RestClient.class).info("-- toString: " + response.toString());
+        LogManager.getLogger(RestClient.class).info("-- Headers: " + response.getHeaders());
+        LogManager.getLogger(RestClient.class).info("-- Status: " + response.getStatus());
+        LogManager.getLogger(RestClient.class).info(
+                "-- Entity: " + response.readEntity(String.class));
+        // Se debe cerrar el flujo de entrada de entidad: response.close();
+        // No es necesario si se utiliza response.readEntity(...)
+        LogManager.getLogger(RestClient.class).info("-- Links: " + response.getLinks());
+        LogManager.getLogger(RestClient.class).info("-- Location: " + response.getLocation());
+
     }
 }
